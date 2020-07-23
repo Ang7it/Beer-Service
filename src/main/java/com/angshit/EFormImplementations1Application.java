c45330bb5866abc1cd37f398d42d27e63a81e0b1
Angshit <angshitghosh@gmail.com>
2020-07-23 21:23:56 +0530

    Initial Commit

---
+++ src/main/java/com/angshit/EFormImplementations1Application.java
@@ -0,0 +1,13 @@
+package com.angshit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EFormImplementations1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EFormImplementations1Application.class, args);
+	}
+
+}
